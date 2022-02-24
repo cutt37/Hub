7ad0f356604e3013501a4fcd9fe76f84fef62e2b
cutt37 <99665719+cutt37@users.noreply.github.com>
2022-02-24 19:00:46 +0800

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function _send() {
+    var _message = document.getElementById("_msg").value;
+    document.getElementById("_output").innerHTML = _message;
+}
+
+function _send2() {
+    var _message = document.getElementById("_msg2").value;
+    document.getElementById("_output2").innerHTML = _message;
+}
+
+function _send3() {
+    var _message = document.getElementById("_msg3").value;
+    document.getElementById("_output3").innerHTML = _message;
+}
+
+function _send4() {
+    var _message = document.getElementById("_msg4").value;
+    document.getElementById("_output4").innerHTML = _message;
+}
